5c918ffb2e24489948877f453f88fa87daa8243d
lioryardeni <liorwhy@gmail.com>
2019-07-28 22:59:03 +0300

    added file directory

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-import youtube_dl
 import os
+
+import youtube_dl
+from os import getcwd, environ, path
 from flask import Flask, request
+from configuration import get_config
+from shutil import copyfile
 
 app = Flask(__name__)
+saved_file_directory = get_config('saved_file_directory')
+project_path = getcwd()
 
 
 @app.route('/send_youtube_url', methods=['POST'])
@@ -22,12 +28,25 @@ def send_youtube_url():
         try:
             video_info = ydl.extract_info(youtube_url)
             video_title = video_info['title']
-            ydl.download([youtube_url])
+            if "/" in video_title:
+                video_title = video_title.replace("/", "_")
+            video_id = video_info['id']
+            file_name = '{}-{}{}'.format(video_title, video_id, ".mp3")
+            full_download_path = '{}/{}'.format(project_path, file_name)
+            full_download_path_ = full_download_path.replace("|", "_")
+            saved_file_path = '{}{}'.format(saved_file_directory, file_name)
+            copyfile(full_download_path_, saved_file_path)
+            os.remove(full_download_path_)
             return "{} downloaded successfully".format(video_title)
         except:
             return "Video is unavailable"
 
 
+@app.route('/health', methods=['GET'])
+def health():
+    return "OK"
+
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', use_reloader=True, port=port)
